881b68a2acc04631b1df02dcb31b63e8718ff97b
roman <roman@localhost.localdomain>
2017-12-10 11:41:02 +0400

    distinguish forward propagate step as a func

--- nn.py
+++ nn.py
@@ -6,33 +6,38 @@ def sigmoid(z):
 def sigmoid_prime(z):
     return sigmoid(z) * (1 - sigmoid(z))
 
-X = np.array([[0,0,1],
-              [0,1,1],
-              [1,0,1],
-              [1,1,1]]).T
+X = np.array([[0,0,1,1],
+              [0,1,0,1],
+              [1,1,1,1]])
 y = np.array([[0,1,1,0]])
+
+def forward_prop(X, weights):
+    z = []
+    a = []
+    
+    z.append(weights[0].dot(X))
+    a.append(sigmoid(z[0]))
+    
+    for i in range(1, len(weights)):
+        z.append(weights[i].dot(a[i-1]))
+        a.append(sigmoid(z[i]))
+    
+    return z, a
 
 def gradient_descent(epochs, rate):
     weights_1 = 2 * np.random.randn(4,3) - 1
     weights_2 = 2 * np.random.randn(1,4) - 1
     for i in range(epochs):
-        z_1 = weights_1.dot(X)
-        a_1 = sigmoid(z_1)
-        #print('a_1.shape - {0}'.format(a_1.shape))
-        z_2 = weights_2.dot(a_1)
-        a_2 = sigmoid(z_2)
-        #print('a_2.shape - {0}'.format(a_2.shape))
+        z, a = forward_prop(X, [weights_1, weights_2])
         
-        delta_2 = (a_2 - y) * sigmoid_prime(z_2)
-        #print('delta_2.shape - {0}'.format(delta_2.shape))
-        delta_1 = (weights_2.T.dot(delta_2)) * sigmoid_prime(z_1)
-        #print('delta_1.shape - {0}'.format(delta_1.shape))
+        delta_2 = (a[1] - y) * sigmoid_prime(z[1])
+        delta_1 = (weights_2.T.dot(delta_2)) * sigmoid_prime(z[0])
         
-        weights_2 = weights_2 - rate * delta_2.dot(a_1.T)
+        weights_2 = weights_2 - rate * delta_2.dot(a[0].T)
         weights_1 = weights_1 - rate * delta_1.dot(X.T)
         
         if (i % (epochs / 10) == 0):
-            print ("Error:" + str(np.mean(np.abs(y - a_2))))
+            print ("Error:" + str(np.mean(np.abs(y - a[1]))))
         
 gradient_descent(100000, 1)
 
