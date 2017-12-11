a12facef579563f66a6f1f57daa85dc412271e1b
roman <roman@localhost.localdomain>
2017-12-11 22:42:25 +0400

    bugfix, refactor needed

--- nn.py
+++ nn.py
@@ -15,9 +15,15 @@ y = np.array([[0,1,1,0]])
             
 class Network:
     def __init__(self, layers, activation=Sigmoid()):
-        self.biases = [(2 * np.random.randn(1, m) - 1) for m in layers[1:]]
+        self.biases = [(2 * np.random.randn(m, 1) - 1) for m in layers[1:]]
         self.weights = [(2 * np.random.randn(n, m) - 1) for n,m in zip(layers[1:], layers[:-1])]
         self.a = activation
+        
+        #print(self.weights[0].shape)
+        #print(self.weights[1].shape)
+        #print(self.biases[0].shape)
+        #print(self.biases[1].shape)
+        #print('\n\n')
         
         
     def forward_prop(self, X):
@@ -43,7 +49,7 @@ class Network:
         w = []
         b = []
         w.append(self.weights[0] - rate * delta[0].dot(X.T))
-        b.append(self.biases[0] - rate * np.sum(delta[0], axis=1))
+        b.append(self.biases[0] - rate * np.sum(delta[0], axis=1).reshape( delta[0].shape[0], 1))
         for i in range(1, len(self.weights)):
             w.append(self.weights[i] - rate * delta[i].dot(a[i-1].T))
             b.append(self.biases[i]  - rate * np.sum(delta[i], axis=1))
@@ -57,9 +63,9 @@ class Network:
             self.update_weights(X, delta, a, rate)
 
             if (i % (epochs / 10) == 0):
-                print ("Error:" + str(np.mean(np.abs(y - a[1]))))
+                print ("Error at epoch " + str(i) + " :" + str(np.mean(np.abs(y - a[1]))))
                 
 
-n = Network([3, 4 ,1])
+n = Network([3, 6 ,1])
 n.gradient_descent(50000, 1, X, y)
 
