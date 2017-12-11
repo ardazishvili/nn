3328d554282a74806efd1f9b23e953f6e51dfcda
roman <roman@localhost.localdomain>
2017-12-11 22:11:47 +0400

    Sigmoid function class introduced

--- nn.py
+++ nn.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+class Sigmoid:
+    def f(self, z):
+        return 1 / (1 + np.exp(-z))
 
-def sigmoid_prime(z):
-    return sigmoid(z) * (1 - sigmoid(z))
+    def prime(self, z):
+        return self.f(z) * (1 - self.f(z))
 
 X = np.array([[0,0,1,1],
               [0,1,0,1],
@@ -13,27 +14,28 @@ y = np.array([[0,1,1,0]])
 
             
 class Network:
-    def __init__(self, layers):
+    def __init__(self, layers, activation=Sigmoid()):
         self.biases = [(2 * np.random.randn(1, m) - 1) for m in layers[1:]]
         self.weights = [(2 * np.random.randn(n, m) - 1) for n,m in zip(layers[1:], layers[:-1])]
+        self.a = activation
         
         
     def forward_prop(self, X):
         z = []
         a = []
         z.append(self.weights[0].dot(X) + self.biases[0])
-        a.append(sigmoid(z[0]))
+        a.append(self.a.f(z[0]))
         for i in range(1, len(self.weights)):
             z.append(self.weights[i].dot(a[i-1]) + self.biases[i])
-            a.append(sigmoid(z[i]))
+            a.append(self.a.f(z[i]))
         return z, a
 
 
     def back_prop(self, y, z, a):
         delta = []
-        delta.append((a[-1] - y) * sigmoid_prime(z[-1]))
+        delta.append((a[-1] - y) * self.a.prime(z[-1]))
         for i in range(1, len(self.weights)):
-            delta.append((self.weights[-i].T.dot(delta[i-1])) * sigmoid_prime(z[-1-i]))
+            delta.append((self.weights[-i].T.dot(delta[i-1])) * self.a.prime(z[-1-i]))
         return list(reversed(delta))
 
 
@@ -57,6 +59,7 @@ class Network:
             if (i % (epochs / 10) == 0):
                 print ("Error:" + str(np.mean(np.abs(y - a[1]))))
                 
+
 n = Network([3, 4 ,1])
 n.gradient_descent(50000, 1, X, y)
 
