38d612f9e9f78c82e76f24818449f4ec93560e78
roman <roman@localhost.localdomain>
2017-12-16 15:34:17 +0400

    intermediate

--- nn.py
+++ nn.py
@@ -15,15 +15,13 @@ class Sigmoid:
 def cost_func(a, b):
     return np.sum(np.power(a - b, 2)) / (2 * len(a))
 
-#X = np.array([[0,0,1,1],
-              #[0,1,0,1],
-              #[1,1,1,1]])
-#y = np.array([[0,1,1,0]])
-
 mnist = fetch_mldata('MNIST original', data_home='/home/roman/experiments/nn/')
-q = 20
+q = 1000
 X = mnist.data.T[:, :q] / np.max(mnist.data)
 y = mnist.target[np.newaxis,:][:, :q]
+
+X_test = mnist.data.T[:, 60000:] / np.max(mnist.data)
+y_test = mnist.target[np.newaxis,:][:, 60000:]
 
             
 class Network:
@@ -34,84 +32,84 @@ class Network:
         
         
     def forward_prop(self, X):
-        
-        #print('\n')
         z = []
         a = [X]
         
-        
         for i, [w, b] in enumerate(zip(self.weights, self.biases)):
-            #print(w.shape)
-            #print(b.shape)
-            #print(a[i].shape)
             z.append(w.dot(a[i]) + b)
             a.append(self.a.f(z[i]))
-        #print('\n')
-        return z, a[1:]
+        return z, a
 
 
-    def back_prop(self, X, y, z, a):
+    def back_prop(self, X, y):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        a = [X] + a
+        
+        z, a = self.forward_prop(X)
         
         delta = (a[-1] - y) * self.a.prime(z[-1])
         nabla_b[-1] = delta
-        print(a[-2].shape)
         nabla_w[-1] = delta.dot(a[-2].T)
         
         for layer in range(2, len(self.weights) + 1):
-            print('sdf')
             z = z[-layer]
             sp = self.a.prime(z)
             delta = self.weights[-layer+1].T.dot(delta) * sp
             nabla_b[-layer] = delta
             nabla_w[-layer] = delta.dot(a[-layer-1].T)
         return nabla_b, nabla_w
-        
-        #for i in range(1, len(self.weights)):
-            #delta.append((self.weights[-i].T.dot(delta[i-1])) * self.a.prime(z[-1-i]))
-        #return list(reversed(delta))
 
 
-    def update_weights(self, X, nabla_b, nabla_w, rate):
-        #nabla_b = [np.sum(nb, axis=1)[:,np.newaxis] for nb in nabla_b]
-        #nabla_w = [np.sum(nb, axis=1)[:,np.newaxis] for nb in nabla_b]
+    def update_weights(self, X_batch, y_batch, rate):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
         
-        
-        self.weights = [w - (rate/len(X)) * nw for w, nw in zip(self.weights, nabla_w)]
-        
-        print(nabla_w[0].shape)
-        print(nabla_w[1].shape)
-        #print(self.biases[0].shape)
-        #print(self.biases[1].shape)
-        self.biases = [b - (rate/len(X)) * nb for b, nb in zip(self.biases, nabla_b)]
-        #print(self.biases[0].shape)
-        #print(self.biases[1].shape)
-        print('\n')
+        for i in range(X_batch.shape[1]):
+            X = X_batch[:, i][:,np.newaxis]
+            y = y_batch[:, i][:,np.newaxis]
+            delta_nabla_b, delta_nabla_w = self.back_prop(X, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            
+        self.weights = [w - (rate/len(X_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+            
+        self.biases = [b - (rate/len(X_batch))*nb for b, nb in zip(self.biases, nabla_b)]
    
    
-    def gradient_descent(self, epochs, rate, X, y, batch_size):
+    def gradient_descent(self, epochs, rate, X, y, batch_size, Xt, yt):
         cost = []
         for i in range(epochs):
             for j in range(int(X.shape[1] / batch_size)):
                 X_batch = X[:, j*batch_size:(j+1)*batch_size]
                 y_batch = y[:, j*batch_size:(j+1)*batch_size]
-                z, a = self.forward_prop(X_batch)
-                nabla_b, nabla_w = self.back_prop(X_batch, y_batch, z, a)
-                self.update_weights(X_batch, nabla_b, nabla_w, rate)
+                self.update_weights(X_batch, y_batch, rate)
 
+
+            self.check_results(Xt, yt)
             z, a = self.forward_prop(X)
-            #cost.append(cost_func(y, a[-1]))
-            #print(cost_func(y, a[-1]))
+            c = cost_func(y, a[-1])
+            cost.append(c)
+            print('cost = ' + str(c) + '\n')
+            
         return cost
+    
+    
+    def check_results(self, X_test, y_test):
+        counter = 0
+        for i in range(X_test.shape[1]):
+            X = X_test[:, i][:,np.newaxis]
+            y = y_test[:, i][:,np.newaxis]
+            z, a = self.forward_prop(X)
+            if (np.argmax(a[-1]) == int(y)):
+                counter +=1
+        print(counter)
                 
-epochs = 1
-batch_size = 10
-n = Network([784, 30, 1])
+epochs = 30
+batch_size = 1000
+n = Network([784, 30, 10])
 
 x = np.arange(epochs)
-y = n.gradient_descent(epochs, 1, X, y, batch_size)
+y = n.gradient_descent(epochs, 3.0, X, y, batch_size, X_test, y_test)
 
 
 #fig, ax = plt.subplots(figsize=(12,8))
