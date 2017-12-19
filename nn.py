b00234e3b5db17ad1b93422a2e9abecea9f6f1e7
roman <roman@localhost.localdomain>
2017-12-19 22:13:27 +0400

    added example of Cross entropy usage

--- nn.py
+++ nn.py
@@ -9,12 +9,25 @@ class Sigmoid:
         z = 1.0 / (1 + np.exp(-z))
         return z
 
-    def prime(self, z):
+    def derivative(self, z):
         return self.f(z) * (1 - self.f(z))
 
+
+class Quadratic_cost:
+    def f(self, a, y):
+        return np.sum(np.power(a - y, 2)) / (2 * len(a))
     
-def cost_func(a, b):
-    return np.sum(np.power(a - b, 2)) / (2 * len(a))
+    def derivative(self, a, y, activation_derivative, arg):
+        return (a - y) * activation_derivative(arg)
+    
+    
+class Cross_entropy_cost:
+    def f(self, a, y):
+        return np.sum(np.nan_to_num(-y * np.log(a) - (1 - y)*np.log(1 - a))) / len(a)
+    
+    def derivative(self, a, y,  activation_derivative, arg):
+        return (a - y)
+        
 
 mnist = fetch_mldata('MNIST original', data_home='/home/roman/experiments/nn/')
 order = np.arange(70000)
@@ -40,11 +53,12 @@ test_data = all_data[60000:]
 
             
 class Network:
-    def __init__(self, layers, activation=Sigmoid()):
+    def __init__(self, layers, activation=Sigmoid(), cost=Quadratic_cost()):
         self.layers_num = len(layers)
         self.biases = [np.random.randn(m, 1) for m in layers[1:]]
         self.weights = [np.random.randn(n, m) for n,m in zip(layers[1:], layers[:-1])]
         self.a = activation
+        self.c = cost
         
         
     def forward_prop(self, X):
@@ -62,13 +76,14 @@ class Network:
         
         zs, a = self.forward_prop(X)
         
-        delta = (a[-1] - y) * self.a.prime(zs[-1])
+        #delta = (a[-1] - y) * self.a.derivative(zs[-1])
+        delta = self.c.derivative(a[-1], y, self.a.derivative, zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = delta.dot(a[-2].T)
 
         for layer in range(2, self.layers_num):
             z = zs[-layer]
-            sp = self.a.prime(z)
+            sp = self.a.derivative(z)
             delta = self.weights[-layer+1].T.dot(delta) * sp
             nabla_b[-layer] = delta
             nabla_w[-layer] = delta.dot(a[-layer-1].T)
@@ -123,9 +138,9 @@ class Network:
                 
 epochs = 30
 batch_size = 10
-n = Network([784, 30, 10])
+n = Network([784, 30, 10], cost=Cross_entropy_cost())
 
-y = n.gradient_descent(training_data, epochs, batch_size, 3.0, test_data=test_data)
+y = n.gradient_descent(training_data, epochs, batch_size, 0.5, test_data=test_data)
 
 
 #fig, ax = plt.subplots(figsize=(12,8))
